cec8babed7e96c472ceddaacb54179f370ffd2a7
Saurav Kashyap <skashyap9934@gmail.com>
2024-08-05 20:01:41 +0530

    Code challenge 1

---
+++ viamagus-cc-1/src/App.jsx
@@ -0,0 +1,15 @@
+// Packages
+import React from "react";
+
+// Components
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ viamagus-cc-1/src/pages/Login.jsx
@@ -0,0 +1,91 @@
+// Packages / Libraries
+import React from "react";
+
+// Stylesheets
+import "../styles/login.css";
+
+// Assests
+import amazonLogo from "../assets/amazon_logo.webp";
+import trees from "../assets/trees.png";
+import tree from "../assets/tree.jpg";
+
+const Login = () => {
+  const [credentails, setCredentails] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const [msg, setMsg] = React.useState({ status: "", email: "", password: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCredentails({ ...credentails, [name]: value });
+    setMsg({ ...msg, [name]: "" });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const regex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    if (regex.test(credentails.email) && credentails.password)
+      setMsg({ ...msg, status: "Login Successful" });
+    else {
+      if (!regex.test(credentails.email))
+        setMsg({ ...msg, email: "Invalid Email Address" });
+      else setMsg({ ...msg, password: "Invalid Password" });
+    }
+  };
+
+  return (
+    <div className="login">
+      <img className="trees-img" src={trees} alt="trees" />
+      <div>
+        <header style={{ width: "100%" }}>
+          <img className="amazon-logo" src={amazonLogo} alt="logo" />
+        </header>
+        <div className="login-body">
+          <div>
+            <p
+              style={{
+                fontSize: "21px",
+                color: "#2BBB22",
+                textAlign: "center",
+              }}
+            >
+              Login
+            </p>
+            <img
+              className="tree"
+              src={tree}
+              alt="tree"
+            />
+          </div>
+          <div className="inputs">
+            <input
+              onChange={handleChange}
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <p className="msg-err">{msg.email}</p>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <p className="msg-err">{msg.password}</p>
+            <button className="submit-btn" type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+          <p className="msg">{msg.status}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
