28b4a7fc2f6c7a08a9e5d40cc8af3a01c8e1b8a2
Saurav Kashyap <skashyap9934@gmail.com>
2024-08-06 15:14:10 +0530

    cc-3

--- viamagus-cc-3/server/functions/api.js
+++ viamagus-cc-3/server/functions/api.js
@@ -1,7 +1,6 @@
 // Packages
 const express = require("express");
 const serverless = require("serverless-http");
-const cors = require("cors");
 
 // Server
 const app = express();
@@ -10,14 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Server Routers
-const appRouter = express.Router();
-
 // RESTful API's
-appRouter.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send(`<h1>Server is UP & Running...</h1>`);
 });
-
-app.use("/.netlify/functions/api", appRouter);
 
 module.exports.handler = serverless(app);
